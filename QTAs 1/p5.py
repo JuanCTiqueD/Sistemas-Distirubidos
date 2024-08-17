37ae8244fcb3065ae0e28ad3f61df9e5039e2456
Juank-t <tiquejuancamilo999@gmail.com>
2024-08-17 08:10:53 -0500

    style: QTAs v5
    Se mejora la legibilidad del codigo

--- QTAs 1/p5.py
+++ QTAs 1/p5.py
@@ -10,6 +10,7 @@ class ListaEnlazada:
         self.cabeza = None
 
     def agregar(self, dato):
+        """Agrega un nuevo nodo al final de la lista."""
         nuevo_nodo = Nodo(dato)
         if not self.cabeza:
             self.cabeza = nuevo_nodo
@@ -20,16 +21,15 @@ class ListaEnlazada:
             actual.siguiente = nuevo_nodo
 
     def buscar_elemento(self, elemento):
+        """Busca un elemento en la lista y devuelve su posición."""
         actual = self.cabeza
-        encontrado = False
         posicion = 0
         while actual:
             if actual.dato == elemento:
-                encontrado = True
-                break
+                return True, posicion  # Retorna verdadero y la posición si se encuentra
             actual = actual.siguiente
             posicion += 1
-        return encontrado, posicion if encontrado else -1
+        return False, -1  # Retorna falso y -1 si no se encuentra
 
 def main():
     if len(sys.argv) <= 2:
@@ -40,9 +40,11 @@ def main():
     elemento_a_buscar = sys.argv[1]
     lista = ListaEnlazada()
     
+    # Agregar elementos a la lista
     for arg in sys.argv[2:]:
         lista.agregar(arg)
     
+    # Buscar el elemento en la lista
     encontrado, posicion = lista.buscar_elemento(elemento_a_buscar)
     if encontrado:
         print(f"Elemento '{elemento_a_buscar}' encontrado en la posición {posicion}.")
